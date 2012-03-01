a1b995f0128dd739027261cdfe2099cba49895dc
Tony DeVincenzi <tonydev@gmail.com>
2012-03-01 16:58:54 -0500

    Adding webGL route

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var AppRouter = Backbone.Router.extend({
 
     routes:{
         "":"map",
+		"gl":"mapGL",
     },
 
     initialize:function () {
@@ -23,7 +24,7 @@ var AppRouter = Backbone.Router.extend({
 		
 	    if (!this.mapView)
 		{
-            this.mapView = new MapGLView({
+            this.mapView = new MapView({
 				collection: this.readingsCollection,
 				vent: self.vent
 			});
@@ -32,6 +33,22 @@ var AppRouter = Backbone.Router.extend({
 			this.readingsCollection.fetch();
         } 
     },
+
+	mapGL:function () {
+		var self = this;
+		
+	    if (!this.mapView)
+		{
+            this.mapView = new MapGLView({
+				collection: this.readingsCollection,
+				vent: self.vent
+			});
+			$('body').append(this.mapView.render().el);
+			this.mapView.start();
+			this.readingsCollection.fetch();
+        }
+    },
+
 });
 
 tpl.loadTemplates(['map', 'mapgl', 'header','sidebar', 'modal'],
