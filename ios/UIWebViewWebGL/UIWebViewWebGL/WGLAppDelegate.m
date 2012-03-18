4aee92aa2ced004bd2858a4734036c576927bc2d
Samuel Luescher <philomat@popkultur.net>
2012-03-18 18:35:26 -0400

    Added iPad test

---
+++ ios/UIWebViewWebGL/UIWebViewWebGL/WGLAppDelegate.m
@@ -0,0 +1,41 @@
+//
+//  WGLAppDelegate.m
+//  UIWebViewWebGL
+//
+//  Created by Nathan de Vries on 27/10/11.
+//  Copyright (c) 2011 Nathan de Vries. All rights reserved.
+//
+
+#import "WGLAppDelegate.h"
+
+@implementation WGLAppDelegate
+
+@synthesize window=_window;
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
+  
+//  Class ADWebView = NSClassFromString(@"ADWebView");
+//  UIWebView* webView = (UIWebView *)[[[ADWebView alloc] initWithFrame:self.window.bounds] autorelease];
+//  [webView setWebGLEnabled:YES];
+    
+  UIWebView* webView = [[[UIWebView alloc] initWithFrame:self.window.bounds] autorelease];
+  
+  id webDocumentView = [webView performSelector:@selector(_browserView)];
+  id backingWebView = [webDocumentView performSelector:@selector(webView)];
+  [backingWebView _setWebGLEnabled:YES];
+  
+  //NSURLRequest* request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://sam-macbookair.media.mit.edu:8000/globe"]];
+  NSURLRequest* request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://sam-macbookair.media.mit.edu:8124/#globe"]];
+  [webView loadRequest:request];
+  
+  UIViewController* viewController = [[[UIViewController alloc] init] autorelease];
+  viewController.view = webView;
+  
+  self.window.rootViewController = viewController;
+  [self.window makeKeyAndVisible];
+  
+  return YES;
+}
+
+@end
--- proxy.pac
+++
@@ -1,9 +0,0 @@
-function FindProxyForURL(url, host) {
-      // our local URLs from the domains below example.com don't need a proxy:
-      if (shExpMatch(url, "http://localhost:3000/plasma/*"))
-      {
-         return "PROXY 18.85.58.21";
-      } else {
-         return "DIRECT";
-      }
-   }
