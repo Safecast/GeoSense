52e0f1a96af40eaa5a1d5a2d46dd7b5b5c217e7d
Tony DeVincenzi <tonydev@gmail.com>
2012-04-15 15:03:54 -0400

    Giving the homepage view a paginator for recent maps

--- public/views/homepage-view.js
+++ public/views/homepage-view.js
@@ -5,11 +5,15 @@ window.HomepageView = Backbone.View.extend({
 	
     events: {
 		'click #createMap': 'createMapButtonClicked',
+		'click #nextMapSet' : 'nextMapSetClicked',
     },
 
     initialize: function(options) {
 	    this.template = _.template(tpl.get('homepage'));
 		this.fetchRecentMaps();
+		this.recentMaps = [];
+		this.recentMapSet = 0;
+		this.numberOfMapsDisplay = 5;
 	},
 
     render: function() {
@@ -39,13 +43,40 @@ window.HomepageView = Backbone.View.extend({
 				
 				for(i=0;i<data.length;i++)
 				{
-					self.$('#mapTable').append('<tr><td>'+data[i].name+'</td><td><a target="_self" href="/'+data[i].mapid+'">geo.media.mit.edu/'+ data[i].mapid +'</a></td><tr>');
+					currMap = '<tr><td>'+data[i].name+'</td><td><a target="_self" href="/'+data[i].mapid+'">geo.media.mit.edu/'+ data[i].mapid +'</a></td><tr>';
+					self.recentMaps.push(currMap);
+					//self.$('#mapTable').append('<tr><td>'+data[i].name+'</td><td><a target="_self" href="/'+data[i].mapid+'">geo.media.mit.edu/'+ data[i].mapid +'</a></td><tr>');
 				}
+				self.showRecentMaps();
 			},
 			error: function() {
 				console.error('failed to fetch unique map');
 			}
 		});	
+	},
+	
+	showRecentMaps: function()
+	{
+		if((this.recentMapSet + this.numberOfMapsDisplay) >= this.recentMaps.length)
+		{
+			this.$('#nextMapSet').fadeOut('fast');
+		}
+		
+		var self = this;
+		self.$('#mapTable').fadeOut('fast',function(){
+			self.$('#mapTable').empty();
+			for(i=self.recentMapSet;i<(self.numberOfMapsDisplay + self.recentMapSet);i++)
+			{
+				self.$('#mapTable').append(self.recentMaps[i]);
+			}
+			self.$('#mapTable').fadeIn('fast');
+		});	
+	},
+	
+	nextMapSetClicked: function()
+	{
+		this.recentMapSet+=this.numberOfMapsDisplay;
+		this.showRecentMaps();
 	},
 
 	createMapButtonClicked: function() {
