371af7df0fe04cc8b066cc40783099eaece1d6ab
Anthony DeVincenzi <tonydev@gmail.com>
2012-04-20 14:08:39 -0400

    Adding /?query= support to routes

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ everyone.now.joinRoom = function(newRoom){
 ////////////////
 
 //Admin Route
-app.get(/^\/[a-zA-Z0-9]{15}(|\/setup)(|\/globe)(|\/map)$/, function(req, res){
+app.get(/^\/[a-zA-Z0-9]{15}(|\/setup)(|\/globe)(|\/map)(|\/?)$/, function(req, res){
    res.sendfile('public/index.html');
 });
 
