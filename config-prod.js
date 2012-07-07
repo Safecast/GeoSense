f1f93114b772d503c9176e68f0561f552dc375a8
Anthony DeVincenzi <tonydev@gmail.com>
2012-07-06 21:20:01 -0400

    Fixing comma in config-prod file

--- config-prod.js
+++ config-prod.js
@@ -1,6 +1,6 @@
 module.exports = {
-	DB_PATH: 'mongodb://safecast:quzBw0k@penny.mongohq.com:10065/app4772485'
-	
+	DB_PATH: 'mongodb://safecast:quzBw0k@penny.mongohq.com:10065/app4772485',
+
 	SMTP_HOST: 'smtp.sendgrid.net',
 	SMTP_PORT: '587',
 	SMTP_USERNAME: 'luescher',			
