48b8473647f3e5bd2b5fe820a888f63fc78fbb5c
Samuel Luescher <philomat@popkultur.net>
2012-08-20 17:43:40 -0400

    Added config-prod.js

---
+++ config-prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+	DB_PATH: 'mongodb://safecast:quzBw0k@penny.mongohq.com:10065/app4772485',
+
+	SMTP_HOST: 'smtp.sendgrid.net',
+	SMTP_PORT: '587',
+	SMTP_USERNAME: 'luescher',			
+	SMTP_PASSWORD: 'Caribbean-Tapas'
+};
