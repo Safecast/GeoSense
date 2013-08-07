4fd8f4fb983531dc7393a8007ebf2a757101a7a9
philomat <philomat@popkultur.net>
2013-08-06 23:24:43 -0400

    Added better config/env options for running server

--- config-custom.js
+++ config-custom.js
@@ -1,6 +1,8 @@
 module.exports = {
 	DB_URI: process.env.DB_URI,
-
+	BASE_URL: process.env.BASE_URL || '/',
+	SERVER_HOST: process.env.SERVER_HOST || '0.0.0.0',
+	SERVER_PORT: process.env.SERVER_PORT || 3000,
 	SMTP_HOST: process.env.SMTP_HOST,
 	SMTP_PORT: process.env.SMTP_PORT,
 	SMTP_USERNAME: process.env.SMTP_USERNAME,			
