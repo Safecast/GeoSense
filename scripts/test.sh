51bbdd04ecbacd94d51db103581d1bdf4fd7e098
philomat <philomat@popkultur.net>
2013-09-16 20:08:38 -0400

    Fixed date conversion and adapted tests to new storage structure

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,5 +4,5 @@ if [ -z "$1" ]
 	then
 		DB_URI="mongodb://localhost/geosense_test" mocha geogoose/test/ test/  --globals toGeoJSON,finalState --timeout 30000
 	else
-		DB_URI="mongodb://localhost/geosense_test" mocha $1
+		DB_URI="mongodb://localhost/geosense_test" mocha $1 --timeout 10000
 fi
